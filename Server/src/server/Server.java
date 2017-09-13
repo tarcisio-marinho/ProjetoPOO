1e2c704552403a6871065c3b549ccd52756f8366
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-09-12 23:25:16 -0300

    loop

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,10 +6,11 @@ public class Server {
     public static void main(String[] args) {
         
         MenuPrincipal menu = new MenuPrincipal();
-        
-        menu.exibirMenu();
-        menu.lerEntrada();
-        menu.switchMenu();
+        while(true){
+            menu.exibirMenu();
+            menu.lerEntrada();
+            menu.switchMenu();
+    
+	}
     }
-
 }
