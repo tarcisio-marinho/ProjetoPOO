993a9db4b24254a2a79210ffe428ae5ff9b558cb
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-09-18 11:22:21 -0300

    update

---
+++ Server/src/server/DAO/ClienteDAO.java
@@ -0,0 +1,19 @@
+
+package server.DAO;
+
+import java.util.List;
+import server.model.Cliente;
+
+public class ClienteDAO {
+
+    private List<Cliente> listaCliente;
+    
+    public boolean buscaLogin(String login, String senha){ //buscar no banco se cliente existe 
+        return false;
+    }
+    
+    public boolean removeCliente(String id){ // remove cliente da lista pesquisando pelo ID
+        return false;
+    }
+    
+}
