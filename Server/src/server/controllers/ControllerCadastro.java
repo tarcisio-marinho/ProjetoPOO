914e5142c26deb7fe4f63454f9c5e88207c3fb7f
JeanDuarte <jean_luccas320@hotmail.com>
2017-09-20 05:11:33 -0300

    Cadastro já pode ser removido e adicionado dependedo do tipo de conta que é passado usando polimorfismo de tipo

--- Server/src/server/controllers/ControllerCadastro.java
+++ Server/src/server/controllers/ControllerCadastro.java
@@ -1,12 +1,49 @@
 
 package server.controllers;
 
+import java.util.ArrayList;
+
 import server.DAO.ClienteDAO;
 import server.DAO.LojaDAO;
+
+import server.model.Conta;
 import server.model.ContaCliente;
 import server.model.ContaLoja;
 
 
+
 public class ControllerCadastro {
+    ClienteDAO bancoCliente = new ClienteDAO();
+    LojaDAO bancoLoja = new LojaDAO();
     
+    ArrayList<ContaCliente> clientes = bancoCliente.getTodos();
+    ArrayList<ContaLoja> lojas = bancoLoja.getTodos();
+    
+	
+	Conta conta = null;
+	
+	public void add(Conta conta) {
+		
+		if(conta instanceof ContaCliente) {
+			
+			clientes.add( (ContaCliente)conta );
+		}else {
+			
+			lojas.add( (ContaLoja)conta );
+		}
+		
+	}
+	
+	public void remove(Conta conta) {
+		
+		if(conta instanceof ContaCliente) {
+			
+			clientes.remove( (ContaCliente)conta );
+		}else {
+			
+			lojas.remove( (ContaLoja)conta );
+		}
+		
+	}
+	
 }
