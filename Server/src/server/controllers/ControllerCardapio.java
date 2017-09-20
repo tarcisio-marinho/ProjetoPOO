c9ed1c17b20b4796e559c7ab5023289c8f436a8c
JeanDuarte <jean_luccas320@hotmail.com>
2017-09-20 08:15:49 -0300

    esse contrller tem apenas um método para buscar o cardapio

--- Server/src/server/controllers/ControllerCardapio.java
+++ Server/src/server/controllers/ControllerCardapio.java
@@ -1,29 +1,35 @@
 package server.controllers;
-import java.util.ArrayList;
+
 import server.DAO.CardapioDAO;
 import server.model.Cardapio;
-import server.model.Produto;
-import server.model.ContaLoja;
+
+
+/*
+ * As únicas coisa que um controller cardapio pode
+ * fazer é retornar o cardapio desejado do banco e
+ * criar um novo, não há necessidade de deletar um cardapio
+ * já o criar cardapio não é um metodo, mas sim a instanciad do objeto
+ * EX: Cardapio cardapio = new Cardapio();
+ * */
 
 public class ControllerCardapio {
-
+    CardapioDAO bancoCardapio = new CardapioDAO();
+	
     
-    public void addProduto(Cardapio cardapio, Produto produto){
-        ArrayList<Produto> produtos = cardapio.getProdutos();
-        produtos.add(produto);
+    //vai no banco de cardapio, busca o cardapio deseja, e retorna para quem chamou
+    //assim podemos fazer qualquer coisa com um cardapio tal como
+    //(remover, adicionar, alterar) um produto do cardapio
+    public Cardapio getCardapio(String id){
+    	Cardapio cardapio = bancoCardapio.buscaCardapio(id);
+    	
+    	if(cardapio != null)
+    		return cardapio;
         
+    	return null;    	    	
     }
     
-    public void alterarProduto(Cardapio cardapio, Produto produto){ 
-        
-        
-    }
     
-    public void removeProduto(Cardapio cardapio, Produto produto){ // loja remove produto do cardapio
-        
-    }
     
-    public Cardapio getCardapio(String id){ // cliente recebe o cardapio
-        return null;
-    }
+    
+    
 }
