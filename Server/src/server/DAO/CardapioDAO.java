bbf066f12693c196fdc24a9373ca386043e9391f
JeanDuarte <jean_luccas320@hotmail.com>
2017-09-20 08:12:22 -0300

    Cardapio finalizado com apenas um método, apenas para buscar um cardapio no banco de cardapios

--- Server/src/server/DAO/CardapioDAO.java
+++ Server/src/server/DAO/CardapioDAO.java
@@ -5,20 +5,27 @@ import server.model.Cardapio;
 public class CardapioDAO {
     private static ArrayList<Cardapio> cardapios = new ArrayList<>();
     
+    
     // quando a loja for criada, ela cria o cardapio
     public void criarCardapio(String id){
         Cardapio cardapio = new Cardapio(id);
         cardapios.add(cardapio);
-    }
+    }    
+    
+    
     
     // cliente que chama para ver os produtos da loja
     // loja chama quando logar e pegar o seu cardapio
     public Cardapio buscaCardapio(String id){ // id da loja
-        for(Cardapio c : cardapios){
-            if(c.getIdLoja().equals(id)){
-                return c;
+        for(Cardapio cardapio : cardapios){
+            if(cardapio.getId().equals(id)){
+                return cardapio;
             }
         }
         return null;
     }
+    
+    
+    
+    
 }
