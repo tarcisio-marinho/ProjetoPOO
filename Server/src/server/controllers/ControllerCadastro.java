ef245112be0b6e38937e773a5483b77ca2117e50
JeanDuarte <jean_luccas320@hotmail.com>
2017-09-20 08:17:21 -0300

    finalizado, salvando uma conta cliente ou loja, como remoção

--- Server/src/server/controllers/ControllerCadastro.java
+++ Server/src/server/controllers/ControllerCadastro.java
@@ -1,7 +1,6 @@
 
 package server.controllers;
 
-
 import server.DAO.ClienteDAO;
 import server.DAO.LojaDAO;
 
@@ -9,36 +8,51 @@ import server.model.Conta;
 import server.model.ContaCliente;
 import server.model.ContaLoja;
 
+/*
+ * Controller para cadastrar e remover uma Conta qualquer.
+ * Para achar uma conta, talvez, seja necessario passar por todas as contas
+ * 
+ * */
 
 
 public class ControllerCadastro {
     ClienteDAO bancoCliente = new ClienteDAO();
     LojaDAO bancoLoja = new LojaDAO();
     
+    
+    //adiciona uma nova conta - passando uma Conta generica(Cliente ou Loja)
 	public void add(Conta conta) {
 		
 		if(conta instanceof ContaCliente) {
-			
-			bancoCliente.getTodos().add( (ContaCliente)conta );
+			bancoCliente.inserir( (ContaCliente)conta );			
 		}else {
 			
-			bancoLoja.getTodos().add( (ContaLoja)conta );
-		}
-		
-		
+			bancoLoja.inserir( (ContaLoja)conta );
+		}		
 	}
 	
-	public void remove(Conta conta) {
-		
-		if(conta instanceof ContaCliente) {
-			
-			bancoCliente.getTodos().remove( (ContaCliente)conta );
-		}else {
-			
-			bancoLoja.getTodos().remove( (ContaLoja)conta );
-		}
-		
-		
-	}
 	
+	//remove buscando pelo registro de login - buscar em  clientes, caso não ache vai em lojas
+	public void remove(String login) {
+		
+		boolean achou = false;
+		
+		
+		for(ContaCliente cliente : bancoCliente.getTodos()) {			
+			if(cliente.getLogin().equals(login)) {//cliente achado
+				bancoCliente.getTodos().remove(cliente);
+				achou = true;
+			}
+		}		
+		
+		if(!achou) { //se não foi achado em cliente buscamos em lojas	
+			for(ContaLoja loja : bancoLoja.getTodos()) {
+				if(loja.getLogin().equals(login)) {//loja achada 
+					bancoLoja.getTodos().remove(loja);
+				}
+			}
+		}
+	}	
+	
+		
 }
