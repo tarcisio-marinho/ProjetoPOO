1e4b1cdaaf80f4be43f9ab852b4e000f1f025e59
JeanDuarte <jean_luccas320@hotmail.com>
2017-09-20 09:05:59 -0300

    Criado o método de deletar cliente, dentro dele é que buscamos se temos como deletar o cliente pesquisado. - cadastrar e Logar estão de forma correta

--- Server/src/server/controllers/ControllerCliente.java
+++ Server/src/server/controllers/ControllerCliente.java
@@ -1,43 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server.controllers;
 
-import server.model.Cliente;
+
+import java.util.ArrayList;
+
 import server.DAO.ClienteDAO;
 import server.model.ContaCliente;
-/**
- *
- * @author Junior
+
+/*
+ * Podemos logar, cadastrar e remover um cliente da base de dados
  */
+
 public class ControllerCliente {
-    private ClienteDAO dao;
+    private ClienteDAO bancoCliente = new ClienteDAO();;
+
     
-    public ControllerCliente(){
-        dao = new ClienteDAO();
+    //O ato de logar é simplismente para buscar o 
+    //clinte para que todas oprações tenham essa relação 
+    public ContaCliente logar(String login, String senha){
+        
+    	//se achar no banco o retorno será a conta
+    	//se não achar o banco retorn null, logo assim retorna null
+    	//para quem quis logar
+    	return bancoCliente.buscar(login);      	
     }
     
-    public boolean logar(String login, String senha){
-        return dao.buscaLogin(login, senha);
-    }
-    /*
-    public boolean cadastrar(ContaCliente cliente1){
-        boolean retorno;
-        retorno = dao.existeLogin(cliente1.getLogin());
-        if(retorno == false){
-            dao.add(cliente1);
-            return true;
-        }else{
-            return false;
-        }
-    }*/
     
-    public Cliente buscarDados(String login){
-        Cliente c;
-        c = dao.buscaCliente(login);
-        return c;
+    
+    //Poderiamos retornar uma boolean informando se foi cadastrado
+    //mas como em OO devemos sempre que aceitar que tudo será verdade
+    //e exceções devem ser retornada, então caberia ai um boolean para informar,
+    //uma vez que tudo está sendo feito no console
+    public void cadastrar(ContaCliente novoCliente){
+                
+        ContaCliente cliente = bancoCliente.buscar( novoCliente.getLogin() );
+        
+        if(cliente != null) {
+        	bancoCliente.inserir(novoCliente);        	
+        }            
+    }
+    
+    
+    
+    //Busca o login no banco de cliente comparando com o que deseja-se deletar
+    //quando é achado apenas usamos o metodo de remover do banco passando o objeto que deve ser removido
+    public void deletar(String login){
+               
+    	for(ContaCliente cliente : (ArrayList<ContaCliente>)bancoCliente.getTodos()) {
+    		if( cliente.getLogin().equals(login) ){
+    			bancoCliente.remover(cliente);
+    		}
+    	}        
     }
 
     
